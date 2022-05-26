2982982b0074f44f496ecedb9295793717544ebb
Катерина <Me@example.com>
2022-05-26 18:49:18 +0300

    main files

---
+++ Lab_4/Building.cs
@@ -0,0 +1,98 @@
+﻿using System;
+
+
+public interface IRent
+{
+	public int CountRent(int price, int quantity, int days);
+}
+
+public interface ITicket
+{
+	public int CountPrice(int price, int customer_age);
+}
+
+public abstract class Building
+{
+	public string Date { get; set; }
+	public int Square { get; set; }
+	public int Floors { get; set; }
+	public Building(string date, int square, int floors)
+	{
+		this.Date = date;
+		this.Square = square;
+		this.Floors = floors;
+	}
+
+	public abstract int capacity();
+}
+
+public class PublicBuilding: Building, ITicket
+{
+
+	public PublicBuilding(string date, int square, int floors): base(date, square, floors) {}
+
+	public int CountPrice(int price, int customer_age) 
+	{
+		if (customer_age <= 6 || customer_age >= 60) 
+		{
+			return price/2;
+		}
+		return price;
+	}
+
+	public override int capacity()
+	{
+		return (this.Square * this.Floors) / 5;
+	}
+}
+
+public class ResidentialBuilding: Building, IRent
+{
+	public ResidentialBuilding(string date, int square, int floors) : base(date, square, floors) { }
+
+	public int CountRent(int price_per_meter, int square, int days) 
+	{
+		return price_per_meter * square * days;
+	}
+
+	public override int capacity()
+	{
+		return (this.Square * this.Floors) / 8;
+	}
+}
+
+public class Cinema: Building, ITicket
+{
+	public Cinema(string date, int square, int floors) : base(date, square, floors) { }
+	public int Price { get; set; }
+
+	public int CountPrice(int price, int customer_age)
+	{
+		if (customer_age <= 6 || customer_age >= 60)
+		{
+			return (price*8)/10;
+		}
+		return price;
+	}
+
+	public override int capacity()
+	{
+		return (this.Square * this.Floors) / 4;
+	}
+
+}
+
+public class Hotel: Building, IRent
+{
+	public Hotel(string date, int square, int floors) : base(date, square, floors) { }
+
+	public int CountRent(int price_per_room, int number_of_rooms, int days) 
+	{
+		return price_per_room * number_of_rooms * days;
+	}
+
+	public override int capacity()
+	{
+		return (this.Square * this.Floors) / 7;
+	}
+}
---
+++ Lab_4/Program.cs
@@ -0,0 +1,37 @@
+﻿using System;
+
+namespace Lab_4
+{
+
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Cinema florencia = new Cinema("12.03.2002", 150, 3);
+            Console.WriteLine(florencia);
+            Console.WriteLine("Дата побудови: " + florencia.Date);
+            Console.WriteLine("Вартiсть квитка (пiльговий): " + florencia.CountPrice(100, 3));
+            Console.WriteLine("Вартiсть квитка (звичайний): " + florencia.CountPrice(100, 30));
+            Console.WriteLine("Мiсткiсть: " + florencia.capacity());
+
+            Hotel California = new Hotel("15.04.1976", 400, 7);
+            Console.WriteLine(California);
+            Console.WriteLine("Дата побудови: " + California.Date);
+            Console.WriteLine("Вартiсть оренди: " + California.CountRent(100, 3, 10));
+            Console.WriteLine("Мiсткiсть: " + California.capacity());
+
+            PublicBuilding museum = new PublicBuilding("23.08.1923", 200, 4);
+            Console.WriteLine(museum);
+            Console.WriteLine("Дата побудови: " + museum.Date);
+            Console.WriteLine("Вартiсть квитка (звичайний): " + museum.CountPrice(50, 34));
+            Console.WriteLine("Вартiсть квитка (пiльговий): " + museum.CountPrice(50, 84));
+            Console.WriteLine("Мiсткiсть: " + museum.capacity());
+
+            ResidentialBuilding house = new ResidentialBuilding("01.01.2012", 150, 20);
+            Console.WriteLine(house);
+            Console.WriteLine("Дата побудови: " + house.Date);
+            Console.WriteLine("Вартiсть оренди: " + house.CountRent(150, 50, 30));
+            Console.WriteLine("Мiсткiсть: " + house.capacity());
+        }
+    }
+}
